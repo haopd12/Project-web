eae64329599234a1d321f42ffbe650aff9d6c739
Trương Văn Hiển <88773748+TruongVanHien194276@users.noreply.github.com>
2023-02-20 16:03:54 +0700

    add data-crawler

--- src/index.js
+++ src/index.js
@@ -20,23 +20,3 @@ route(app);
 app.listen(port, () => {
   console.log(`Example app listening at https://localhost:${port}`)
 })
-
-
-// open the database
-// let db = new sqlite3.Database('./models/database.db', sqlite3.OPEN_READWRITE, (err) => {
-//   if (err) {
-//     console.error(err.message);
-//   }
-//   console.log('Connected to the chinook database.');
-// });
-
-// db.serialize(() => {
-//   db.each(`SELECT *
-//            FROM Novel
-//            `, (err, row) => {
-//     if (err) {
-//       console.error(err.message);
-//     }
-//     console.log(row.NID + "\t" + row.Theme);
-//   });
-// });
